6fdbd6f2adb8157cca0e5dc1bd112961c0400435
Dipto Biswas <diptobiswas@MacBook-Pro-93.local>
2023-03-21 11:59:16 -0400

    updated for bylaw

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: 'Hi, what would you like to learn about?',
         type: 'apiMessage',
       },
     ],
@@ -153,7 +153,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Chat With Your Docs
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -267,7 +267,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'What is this document about?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -298,11 +298,6 @@ export default function Home() {
             </div>
           </main>
         </div>
-        <footer className="m-auto">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
-          </a>
-        </footer>
       </Layout>
     </>
   );
